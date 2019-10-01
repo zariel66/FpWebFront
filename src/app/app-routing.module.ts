185399e3777d104809994dddbdb0edf65449fe61
Dimitri Laaz <dimitri.laaz.1990@gmail.com>
2019-10-01 17:13:55 -0500

    added components and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateModule} from './template/template.module'
+import { NcguiasComponent } from './ncguias/ncguias/ncguias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/ncguias', pathMatch: 'full'
+  },
+  {
+    path:'ncguias',component: NcguiasComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations:[NcguiasComponent],
+  imports: [
+    RouterModule.forRoot(routes),
+    TemplateModule
+  ],
+  exports: [RouterModule
+  ,TemplateModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/ncguias/ncguias/ncguias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ncguias',
+  templateUrl: './ncguias.component.html',
+  styleUrls: ['./ncguias.component.css']
+})
+export class NcguiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
