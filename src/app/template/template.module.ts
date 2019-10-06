c88f3673767738dfff27c4765a227eff0af25704
Dimitri Laaz <dimitri.laaz.1990@gmail.com>
2019-10-06 12:57:04 -0500

    added package material design

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   exports:
   [
-    SidebarComponent
+    SidebarComponent,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class TemplateModule { }
