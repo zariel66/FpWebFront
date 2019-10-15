e5300215c3ce7be8b872737e8921c8e5e60fd41b
Dimitri Laaz <dimitri.laaz.1990@gmail.com>
2019-10-14 22:15:16 -0500

    added datepicker control, pending to be tested and load css from node modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TemplateModule} from './template/template.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TemplateModule } from './template/template.module'
 import { NcguiasComponent } from './ncguias/ncguias/ncguias.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
 
 const routes: Routes = [
   {
-    path:'',redirectTo:'/ncguias', pathMatch: 'full'
+    path: '', redirectTo: '/ncguias', pathMatch: 'full'
   },
   {
-    path:'ncguias',component: NcguiasComponent
+    path: 'ncguias', component: NcguiasComponent
   }
 ];
 
 @NgModule({
-  declarations:[NcguiasComponent],
+  declarations: [NcguiasComponent],
   imports: [
     RouterModule.forRoot(routes),
-    TemplateModule
+    FormsModule,
+    ReactiveFormsModule,
+    TemplateModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot()
   ],
   exports: [RouterModule
-  ,TemplateModule
+    , TemplateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TemplateModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule
   ]
 })
 export class AppRoutingModule { }
